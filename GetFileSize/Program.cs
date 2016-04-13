0119f542bbed509f6d78a661edf70bfd431c4d9f
Nick from wWindows <cyberdak095@gmail.com>
2016-04-14 00:48:02 +0300

    The New project created

---
+++ GetFileSize/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFileSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("I'm a program!");
+            Console.ReadLine();
+        }
+    }
+}

b186d4a186c0dab39268dcc2f86cef1bc216d066
Nick on Windows <cyberdak095@gmail.com>
2016-04-15 15:15:39 +0300

    Added scan dir feature

--- GetFileSize/Program.cs
+++ GetFileSize/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,70 @@ namespace GetFileSize
     {
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("I'm a program!");
+            Console.WriteLine("I'm a program!");
+            string input = getInput();
+            Scanner s = new Scanner(input);
+            //Display d = new Display();
             Console.ReadLine();
+        }
+
+        private static string getInput()
+        {
+            
+            Console.WriteLine("Enter your username: ");
+            string line = Console.ReadLine();            
+            return line;
+        }
+    }
+
+    class Scanner
+    { 
+        public string homeDir = "C:\\Users\\";
+
+        public Scanner(string username)
+        {
+            homeDir =String.Concat(homeDir, username, "\\");
+            Console.WriteLine(homeDir);
+            //showDir();
+            scan();
+        }
+
+        private void scan()
+        {
+            if (Directory.Exists(homeDir))
+            {
+                Console.Out.WriteLine("Scanning...");
+                Console.WriteLine("subdirs:");
+                string[] dirs = Directory.GetDirectories(homeDir);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("files:");
+                string[] files = Directory.GetFiles(homeDir);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Home directory does not exists!");
+            }
+        }
+
+        void showDir()
+        {
+            Console.Out.WriteLine("Going to scan " + homeDir + "\n");
+        }
+    }
+
+    class Display
+    {
+        public Display()
+        {
+            Console.Out.WriteLine("I will display smth soon!");
         }
     }
 }
